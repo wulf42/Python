7c270b9d2e15fac2e73e4d3a8ee142df3de4c873
wulf <wojtekpako2@gmail.com>
2020-03-04 18:08:08 +0100

    lekcja1(03-04-2020) commit 3

--- lekcja1(04-03-2020).py
+++ lekcja1(04-03-2020).py
@@ -14,3 +14,4 @@ print(list(range(2,12,2)))
 print(list(range(0,-12,-2)))
 print(imie[2])
 print(imie[::2])
+#break idzie na koniec pętli, continue przechodzi o 1 dalej, coś jak i++
---
+++ zadania.py
@@ -0,0 +1,34 @@
+print("Zadanie1: ")
+calkowita=4
+striing="Wyraz"
+ulamek=12.53
+print(calkowita)
+print(striing)
+print(ulamek)
+
+print("Zadanie2: ")
+a=5
+b=4
+print("1.Dodawanie: "+str(a)+" + "+str(b)+" = "+str(a+b))
+print("2.Odejmowanie: "+str(a)+" - "+str(b)+" = "+str(a-b))
+print("3.Mnożenie: "+str(a)+" * "+str(b)+" = "+str(a*b))
+print("4.Dzielenie: "+str(a)+" / "+str(b)+" = "+str(a/b))
+print("5.Dzielenie całkowite: "+str(a)+" / "+str(b)+" = "+str(a//b))
+print("6.Reszta z dzielenia: "+str(a)+" / "+str(b)+" = "+str(a%b))
+print("7.Potęgowanie: "+str(a)+" do potęgi "+str(b)+" = "+str(pow(a,b)))
+print("Zadanie3: ")
+print("operatory przedrostkowe")
+from math import *
+print("Zadanie4: ")
+print("e^10= "+str(pow(e,10)))
+print("|3.55|= "+str(abs(3.55))) #moze modf
+print("|4.8|= "+str(abs(4.8)))
+
+print("Zadanie5: ")
+imie="WOJTEK"
+nazwisko="PAKOCA"
+print(imie.capitalize()+" "+nazwisko.capitalize())
+print("Zadanie6: ")
+tekst="tekst piosenki la la piosenka lo la la la la costam la la"
+print(tekst.count("la"))
+print("Zadanie7: ")
