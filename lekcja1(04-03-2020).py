341816d2b2022b60f1d0107afabb8ebff0fe821d
wulf <wojtekpako2@gmail.com>
2020-03-04 17:18:00 +0100

    lekcja1(03-04-2020) commit 2

--- lekcja1(04-03-2020).py
+++ lekcja1(04-03-2020).py
@@ -1,4 +1,16 @@
 liczba =10
 if 0 < liczba < 11:
     print("OK")
-    
+
+imie="Ariadna"
+for litera in imie:
+    print(litera, end="")
+print()
+range (5) #start, stop step
+#stop -> stop-step
+print(range(5))
+print(type(range(5)))
+print(list(range(2,12,2)))
+print(list(range(0,-12,-2)))
+print(imie[2])
+print(imie[::2])
