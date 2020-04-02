dd643496b516c0fe3ffc331f4eab1cb993ba93d5
Wulf <wojtekpako2@gmail.com>
2020-04-02 16:27:43 +0200

    Exercise 4

---
+++ 04 Divisors.py
@@ -0,0 +1,7 @@
+liczba = int(input("Podaj liczbę: "))
+x=liczba
+print(f"Dzielniki liczby {liczba} to: ")
+for a in range(1,liczba+1):    
+    if liczba % a == 0:
+        x=int(liczba/a)
+        print(x,end=' ')   
