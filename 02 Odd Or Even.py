848a516ea77d59e38383f89ca143f8dd77f1d1e9
Wulf <wojtekpako2@gmail.com>
2020-04-02 13:48:28 +0200

    Exercise 2

---
+++ 02 Odd Or Even.py
@@ -0,0 +1,12 @@
+liczba = int(input("Podaj liczbę: "))
+if liczba % 2 == 0 and liczba % 4 != 0:
+    print(f"Liczba {liczba} jest parzysta")
+elif liczba % 4 == 0:  
+    print(f"Liczba {liczba} jest parzysta, wielokrotność 4")
+else:
+    print(f"Liczba {liczba} jest nieparzysta")
+dzielnik = int(input("Podaj dzielnik: "))
+if liczba % dzielnik == 0:
+    print(f"Iloraz {liczba} : {dzielnik} jest parzysty")
+else:
+    print(f"Iloraz {liczba} : {dzielnik} jest nieparzysty")
