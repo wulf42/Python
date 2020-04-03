003a40dfb5e4ac04104da55c8f4163e233e97dda
Wulf <wojtekpako2@gmail.com>
2020-04-03 15:48:23 +0200

    Exercise 2

--- 02 Odd Or Even.py
+++ 02 Odd Or Even.py
@@ -1,7 +1,7 @@
 liczba = int(input("Podaj liczbę: "))
 if liczba % 2 == 0 and liczba % 4 != 0:
     print(f"Liczba {liczba} jest parzysta")
-elif liczba % 4 == 0:  
+elif liczba % 4 == 0:
     print(f"Liczba {liczba} jest parzysta, wielokrotność 4")
 else:
     print(f"Liczba {liczba} jest nieparzysta")
