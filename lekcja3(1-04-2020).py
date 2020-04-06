652e941401d5f03c15e169087248346fef3d2663
Wulf <wojtekpako2@gmail.com>
2020-04-06 15:17:38 +0200

    lekcja 06.04.2020 15:17

--- lekcja3(1-04-2020).py
+++ lekcja3(1-04-2020).py
@@ -1,4 +1,5 @@
 # Zadanie 1 (Wygeneruj liczby podzielne przez 4 i zapisz je do pliku.)
+print("Zadanie 3: Generuje liczby i wstawia je do pliku")
 plik = open("liczby.txt", "w")
 for liczba in range(1000):
     if liczba % 4 == 0:
@@ -6,12 +7,14 @@ for liczba in range(1000):
 plik.close()
 
 # Zadanie 2 (Odczytaj plik z poprzedniego zadania i wyświetl jego zawartość w konsoli.)
+print("Zadanie 2: ")
 plik = open("liczby.txt", "r")
 wiersze = plik.readlines()
 print(wiersze)
 plik.close()
 
 # Zadanie 3 (Wykorzystując komendę with zapisz kilka linijek tekstu do pliku a następnie wyświetl je na ekranie.)
+print("Zadanie 3: ")
 with open("tekst.txt", "w") as plik:
     for a in range(10):
         plik.write("Przykładowy tekst zapisywany do pliku\n")
@@ -25,49 +28,41 @@ with open("tekst.txt", "r") as plik:
 #   wyświetl_produkt() – drukuje informacje o produkcie na ekranie
 #   ile_produktu() – informacje ile danego produktu ma być czyli ilosc + jednostka_miary np. 1 szt., 3 kg itd.
 #   ile_kosztuje() – oblicza ile kosztuje dana ilość produktu np. 3 kg ziemniaków a cena_jed wynosi 2 zł/kg wówczas funkcja powinna zwrócić wartość 3*2)
-
+print("Zadanie 4: ")
 
 class NaZakupy:
-    def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed):
-        self.nazwa_produktu = nazwa_produktu
-        self.ilosc = ilosc
+    def __init__(self, nazwa_produktu, ilosc,jednostka_miary,cena_jed):
+        
+        self.nazwa_produktu=nazwa_produktu
+        self.ilosc=ilosc
         self.jednostka_miary = jednostka_miary
         self.cena_jed = cena_jed
 
     def wyświetl_produkt(self):
-        print(
-            f"{self.nazwa_produktu} {self.ilosc} {self.jednostka_miary} {self.cena_jed}")
+        return self.nazwa_produktu
 
     def ile_produktu(self):
-        print(f"{self.ilosc} {self.jednostka_miary}")
+        return self.ilosc, self.jednostka_miary
+        
 
     def ile_kosztuje(self):
-        print(f"{self.ilosc*self.cena_jed} zł")
+        return self.ilosc*self.cena_jed
 
+     
+mleko= NaZakupy("mleko",6,"l",2.99)
+chleb= NaZakupy("chleb",1,"szt",2.99)
+cukier= NaZakupy("cukier",3,"kg",2.99)
 
-def konstruktor():
-    Mleko = NaZakupy("Mleko", 2, "L", 3.99)
-    Chleb = NaZakupy("Chleb", 1, "Szt", 2.99)
-    Woda = NaZakupy("Chleb", 6, "L", 1.19)
-    Cukier = NaZakupy("Cukier", 6, "kg", 4.99)
+print(mleko.wyświetl_produkt(), mleko.ile_produktu(), mleko.ile_kosztuje())
 
-    Mleko.wyświetl_produkt()
-    Chleb.wyświetl_produkt()
-    Woda.wyświetl_produkt()
-    Cukier.wyświetl_produkt()
+print(chleb.wyświetl_produkt(),chleb.ile_produktu(),chleb.ile_kosztuje())
 
-    Mleko.ile_produktu()
-    Chleb.ile_produktu()
-    Woda.ile_produktu()
-    Cukier.ile_produktu()
+print(cukier.wyświetl_produkt(), cukier.ile_produktu(), cukier.ile_kosztuje())
 
-    Mleko.ile_kosztuje()
-    Chleb.ile_kosztuje()
-    Woda.ile_kosztuje()
-    Cukier.ile_kosztuje()
-
-
-konstruktor()
+ 
+ 
+ 
+ 
 # Zadanie 5
 # Utwórz klasę, która definiuje ciągi arytmetyczne. Wartości powinny być przechowywane jako atrybut. Klasa powinna mieć metody:
 # wyświetl_dane – drukuje elementy na ekran
