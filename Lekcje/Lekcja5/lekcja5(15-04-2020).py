753a6489cb37dd77a261537380a215540214a982
Wulf <wojtekpako2@gmail.com>
2020-04-15 20:23:42 +0200

    15.04.2020, 20:23

--- Lekcje/Lekcja5/lekcja5(15-04-2020).py
+++ Lekcje/Lekcja5/lekcja5(15-04-2020).py
@@ -1,1 +1,11 @@
-print("Lekcja 5")
+import numpy as np
+import os
+os.system('cls')
+# import numpy z indexem np.
+print("Zadanie 1: ")
+# Za pomocą funkcji arange stwórz tablicę Numpy składającą się 20 kolejnych wielokrotności liczby 2.
+a = np.arange(1, 21, 1)
+a = 2**a
+print(a)
+print("Zadanie 2: ")
+# Stwórz listę składającą się z wartości zmiennoprzecinkowych a następnie zapisz do innej zmiennej jej kopię przekonwertowaną na typ int64.
