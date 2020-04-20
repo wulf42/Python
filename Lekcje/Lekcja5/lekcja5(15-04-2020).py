89d88193de85657affb8108f2fd25e49508db4b9
Wulf <wojtekpako2@gmail.com>
2020-04-20 19:31:17 +0200

    .

--- Lekcje/Lekcja5/lekcja5(15-04-2020).py
+++ Lekcje/Lekcja5/lekcja5(15-04-2020).py
@@ -116,16 +116,17 @@ funkcjazad8(1)
 
 
 print("Zadanie 9: ")
-# Wykorzystaj poznane na zajęciach funkcje biblioteki Numpy i stwórz macierz 5x5, 
+# Wykorzystaj poznane na zajęciach funkcje biblioteki Numpy i stwórz macierz 5x5,
 # która będzie zawierała kolejne wartości ciągu Fibonacciego.
+
 
 def funkcjazad9(n):
     a = [1, 1]
     for i in range(n*n-2):
-        a.append(a[i] + a[(i + 1)])           
+        a.append(a[i] + a[(i + 1)])
     fi = np.asarray(a)
-    b=np.reshape(fi,(n,n))
+    b = np.reshape(fi, (n, n))
     print(b)
 
-funkcjazad9(5)
 
+funkcjazad9(5)
