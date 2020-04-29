4e4a030099ae5f49ae895670d5571ebac27fd067
Wulf <wojtekpako2@gmail.com>
2020-04-29 17:36:03 +0200

    .

--- Lekcje/Lekcja7/lekcja7(28-04-2020).py
+++ Lekcje/Lekcja7/lekcja7(28-04-2020).py
@@ -17,17 +17,23 @@ print(df)
 # zadanie 2
 print("Zadanie 2")
 # tylko te rekordy gdzie liczba nadanych imion była większa niż 1000 w danym roku
+print("a:")
 print(df[df['Liczba'] > 1000])
 # tylko rekordy gdzie nadane imię jest takie jak Twoje
+print("b:")
 print(df[df['Imie'] == 'WOJTEK'])
 # sumę wszystkich urodzonych dzieci w całym danym okresie
+print("c:")
 print(df.agg({'Liczba': ['sum']}))
 # sumę dzieci urodzonych w latach 2000-2005
+print("d:")
 print(df[df['Rok'] <= 2005].agg({'Liczba': [sum]}))
 # sumę urodzonych chłopców i dziewczynek
+print("e:")
 print(df[df['Plec'] == "M"].agg({'Liczba': [sum]}))
 print(df[df['Plec'] == "K"].agg({'Liczba': [sum]}))
 # najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok)
+print("f:")
 print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).nth(0))
 
 
