2819b48cdf4fe36a9757c95d483c97b98a22a541
Wulf <wojtekpako2@gmail.com>
2020-04-29 17:30:00 +0200

    .

--- Lekcje/Lekcja7/lekcja7(28-04-2020).py
+++ Lekcje/Lekcja7/lekcja7(28-04-2020).py
@@ -28,6 +28,25 @@ print(df[df['Rok'] <= 2005].agg({'Liczba': [sum]}))
 print(df[df['Plec'] == "M"].agg({'Liczba': [sum]}))
 print(df[df['Plec'] == "K"].agg({'Liczba': [sum]}))
 # najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok)
-print("x")
+
+
+
+print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).nth(0))
+# # pierwsze 2 wiersze każdej grupy
+print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).nth([0, 1]))
+new_df = df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec'])
+for index, group in enumerate(new_df, start=1):
+    print(f"{index} {group[0]}")
+    print(f" {group[1].iloc[0]['Imie']}", end='')
+    print(f" {group[1].iloc[0]['Liczba']}")
+
+
+
+
+
+
+
+
+
 # najbardziej popularne imię dziewczynki i chłopca w całym danym okresie
 print("y")
