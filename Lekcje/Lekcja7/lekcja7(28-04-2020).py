2cece8d18f84e48ebafac1e2a8d4edc00da6b90e
Wulf <wojtekpako2@gmail.com>
2020-05-01 23:22:20 +0200

    .

--- Lekcje/Lekcja7/lekcja7(28-04-2020).py
+++ Lekcje/Lekcja7/lekcja7(28-04-2020).py
@@ -6,39 +6,39 @@ import os
 os.system('cls')
 
 
-# # zadanie 1
-# print("Zadanie 1")
-# xlsx = pd.ExcelFile(
-#     'D:\programowanie\wizualizacja danych\Lekcje\Lekcja7\imiona.xlsx')
-# df = pd.read_excel(xlsx, 'Arkusz1')
-# print(df)
+# zadanie 1
+print("Zadanie 1")
+xlsx = pd.ExcelFile(
+    'D:\programowanie\wizualizacja danych\Lekcje\Lekcja7\imiona.xlsx')
+df = pd.read_excel(xlsx, 'Arkusz1')
+print(df)
 
 
-# # zadanie 2
-# print("Zadanie 2")
-# # tylko te rekordy gdzie liczba nadanych imion była większa niż 1000 w danym roku
-# print("a:")
-# print(df[df['Liczba'] > 1000])
-# # tylko rekordy gdzie nadane imię jest takie jak Twoje
-# print("b:")
-# print(df[df['Imie'] == 'WOJTEK'])
-# # sumę wszystkich urodzonych dzieci w całym danym okresie
-# print("c:")
-# print(df.agg({'Liczba': ['sum']}))
-# # sumę dzieci urodzonych w latach 2000-2005
-# print("d:")
-# print(df[df['Rok'] <= 2005].agg({'Liczba': [sum]}))
-# # sumę urodzonych chłopców i dziewczynek
-# print("e:")
-# print(df[df['Plec'] == "M"].agg({'Liczba': [sum]}))
-# print(df[df['Plec'] == "K"].agg({'Liczba': [sum]}))
-# # najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok)
-# print("f:")
-# print(df.sort_values('Liczba', ascending=False).groupby(
-#     ['Rok', 'Plec']).nth(0))
-# # najbardziej popularne imię dziewczynki i chłopca w całym danym okresie
-# print("g:")
-# print(df.groupby(['Plec','Imie']).agg({'Liczba':['sum']}).sort_values(('Liczba','sum'),ascending=False).iloc[[0,1]])
+# zadanie 2
+print("Zadanie 2")
+# tylko te rekordy gdzie liczba nadanych imion była większa niż 1000 w danym roku
+print("a:")
+print(df[df['Liczba'] > 1000])
+# tylko rekordy gdzie nadane imię jest takie jak Twoje
+print("b:")
+print(df[df['Imie'] == 'WOJTEK'])
+# sumę wszystkich urodzonych dzieci w całym danym okresie
+print("c:")
+print(df.agg({'Liczba': ['sum']}))
+# sumę dzieci urodzonych w latach 2000-2005
+print("d:")
+print(df[df['Rok'] <= 2005].agg({'Liczba': [sum]}))
+# sumę urodzonych chłopców i dziewczynek
+print("e:")
+print(df[df['Plec'] == "M"].agg({'Liczba': [sum]}))
+print(df[df['Plec'] == "K"].agg({'Liczba': [sum]}))
+# najbardziej popularne imię dziewczynki i chłopca w danym roku ( czyli po 2 rekordy na rok)
+print("f:")
+print(df.sort_values('Liczba', ascending=False).groupby(
+    ['Rok', 'Plec']).nth(0))
+# najbardziej popularne imię dziewczynki i chłopca w całym danym okresie
+print("g:")
+print(df.groupby(['Plec','Imie']).agg({'Liczba':['sum']}).sort_values(('Liczba','sum'),ascending=False).iloc[[0,1]])
 
 # zadanie 3
 print("Zadanie 3")
