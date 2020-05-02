1e21f23ae6d2720c17ce5d2e41777e646034d537
Wulf <wojtekpako2@gmail.com>
2020-05-02 13:45:06 +0200

    .

--- Lekcje/Lekcja8(9_pandas_2)/lekcja8(29-04-2020).py
+++ Lekcje/Lekcja8(9_pandas_2)/lekcja8(29-04-2020).py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 os.system('cls')
+# Zadanie 1 Stwórz wykres liniowy, który wyświetli liczbę urodzonych dzieci dla każdego roku.
 
-# korzystając z funkcji random oraz date_range możemy wygenerować szereg czasowy danych
-ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2015', periods=1000))
-# funkcja biblioteki Pandas generująca skumulowana sumę kolejnych elementów
-ts = ts.cumsum()
-print(ts)
-ts.plot()
-plt.show()
+# Zadanie 2 Stwórz wykres słupkowy, który wyświetli liczbę urodzonych chłopców i dziewczynek z całego zbioru.
+
+# Zadanie 3 Wykres kołowy z wartościami % ukazującymi ilość urodzonych chłopców i dziewczynek w ostatnich 5 latach z datasetu.
+
+# Zadanie 4 Z repozytorium UCI (http://archive.ics.uci.edu/ml/index.php) pobierz dataset Iris i za pomocą wykresu punktowego (scattered) wyświetl wartość 2 wybranych cech tego datasetu. Dla każdego rodzaju kwiatu użyj innego koloru na wykresie. Przykład można znaleźć w galerii wykresów biblioteki matplotlib - link w materiałach matplotlib.
+
+# Zadanie 5 Wyświetl na pomocą wykresu słupkowego ilość złożonych zamówień przez poszczególnych sprzedawców (zbiór danych zamówienia.csv).
