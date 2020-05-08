1811d81dc153b16c68b06a5a0177b8e1136212c1
Wulf <wojtekpako2@gmail.com>
2020-05-08 14:00:54 +0200

    .

--- Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
+++ Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
@@ -5,6 +5,7 @@ import os
 # Na wykresie wyświetl wykres liniowy funkcji f(x) = 1/x dla x ϵ [1, 20].
 # Dodaj etykietę do linii wykresu i wyświetl legendę.
 # Dodaj odpowiednie etykiety do osi wykresu (‘x’, ’f(x)’) oraz ustaw zakres osi na (0, 1) oraz (1, długość wektora x).
+
 a = []
 for i in range(1, 22):
     a.append(1/i)
@@ -12,8 +13,10 @@ plt.plot(a)
 plt.xlabel('funkcja 1/x dla x ϵ [1, 20]')
 plt.xticks(np.arange(0, 22, step=1))
 plt.show()
+
 # Zadanie 2
 # Zmodyfikuj wykres z zadania 1 tak, żeby styl wykresu wyglądał tak jak na poniższym zrzucie ekranu.
+
 a = []
 for i in range(1, 22):
     a.append(1/i)
@@ -22,8 +25,10 @@ plt.plot(a, 'g>')
 plt.title('Funkcja 1/x dla x ϵ [1, 20]')
 plt.xticks(np.arange(0, 22, step=1))
 plt.show()
+
 # Zadanie 3
 # Na jednym wykresie wygeneruj wykresy funkcji sin(x) oraz cos(x) dla x ϵ [0, 30] z krokiem 0.1. Dodaj etykiety i legendę do wykresu.
+
 x = np.arange(0, 30, 0.1)
 s = np.sin(x)
 c = np.cos(x)
@@ -32,6 +37,18 @@ plt.plot(x, c, label='cos(x)')
 plt.title('Funkcje sin(x) i cos(x)  dla x ϵ [0, 30]')
 plt.legend(loc=4)
 plt.show()
+
 # Zadanie 4
 # Dodaj drugi wykres funkcji sinus do zadania 3 i zmodyfikuj parametry funkcji,
 # tak aby osiągnąć efekt podobny do tego na poniższym zrzucie ekranu.
+
+x = np.arange(0, 30, 0.1)
+s = np.sin(x)*-1
+s2 = np.sin(x)+2
+plt.plot(x, s, label='sin(x)')
+plt.plot(x, s2, label='sin(x)')
+plt.title('Wykres sin(x),sin(x)')
+plt.ylabel('sin(x)')
+plt.xlabel('x')
+plt.legend(loc=6)
+plt.show()
