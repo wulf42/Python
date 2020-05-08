982606859595c746e15fe9fb96ece464944b5ee9
Wulf <wojtekpako2@gmail.com>
2020-05-08 13:28:12 +0200

    .

--- Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
+++ Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
@@ -1,4 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-os.system('cls')
+
+# Zadanie 1
+# Na wykresie wyświetl wykres liniowy funkcji f(x) = 1/x dla x ϵ [1, 20].
+# Dodaj etykietę do linii wykresu i wyświetl legendę.
+# Dodaj odpowiednie etykiety do osi wykresu (‘x’, ’f(x)’) oraz ustaw zakres osi na (0, 1) oraz (1, długość wektora x).
+a=[]
+for i in range(1,22):
+    a.append(1/i)
+plt.plot(a)
+plt.xlabel('funkcja 1/x dla x ϵ [1, 20]')
+plt.xticks(np.arange(0, 22, step=1))
+plt.show()
