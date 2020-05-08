a2e88f682130552f14126958959b428cad3d856a
Wulf <wojtekpako2@gmail.com>
2020-05-08 13:51:27 +0200

    .

--- Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
+++ Lekcje/Lekcja9(10_matplotlib_1)/lekcja(06-05-2020).py
@@ -5,30 +5,33 @@ import os
 # Na wykresie wyświetl wykres liniowy funkcji f(x) = 1/x dla x ϵ [1, 20].
 # Dodaj etykietę do linii wykresu i wyświetl legendę.
 # Dodaj odpowiednie etykiety do osi wykresu (‘x’, ’f(x)’) oraz ustaw zakres osi na (0, 1) oraz (1, długość wektora x).
-# a=[]
-# for i in range(1,22):
-#     a.append(1/i)
-# plt.plot(a)
-# plt.xlabel('funkcja 1/x dla x ϵ [1, 20]')
-# plt.xticks(np.arange(0, 22, step=1))
-# plt.show()
+a = []
+for i in range(1, 22):
+    a.append(1/i)
+plt.plot(a)
+plt.xlabel('funkcja 1/x dla x ϵ [1, 20]')
+plt.xticks(np.arange(0, 22, step=1))
+plt.show()
 # Zadanie 2
 # Zmodyfikuj wykres z zadania 1 tak, żeby styl wykresu wyglądał tak jak na poniższym zrzucie ekranu.
-# a=[]
-# for i in range(1,22):
-#     a.append(1/i)
-# plt.plot(a,'g:')
-# plt.plot(a,'g>')
-# plt.title('Funkcja 1/x dla x ϵ [1, 20]')
-# plt.xticks(np.arange(0, 22, step=1))
-# plt.show()
+a = []
+for i in range(1, 22):
+    a.append(1/i)
+plt.plot(a, 'g:')
+plt.plot(a, 'g>')
+plt.title('Funkcja 1/x dla x ϵ [1, 20]')
+plt.xticks(np.arange(0, 22, step=1))
+plt.show()
 # Zadanie 3
 # Na jednym wykresie wygeneruj wykresy funkcji sin(x) oraz cos(x) dla x ϵ [0, 30] z krokiem 0.1. Dodaj etykiety i legendę do wykresu.
-# x = np.arange(0, 30, 0.1)
-# s = np.sin(x)
-# c = np.cos(x)    
-# plt.plot(x, s, label='sin(x)')
-# plt.plot(x, c, label='cos(x)')
-# plt.title('Funkcje sin(x) i cos(x)  dla x ϵ [0, 30]')
-# plt.legend(loc=4)
-# plt.show()
+x = np.arange(0, 30, 0.1)
+s = np.sin(x)
+c = np.cos(x)
+plt.plot(x, s, label='sin(x)')
+plt.plot(x, c, label='cos(x)')
+plt.title('Funkcje sin(x) i cos(x)  dla x ϵ [0, 30]')
+plt.legend(loc=4)
+plt.show()
+# Zadanie 4
+# Dodaj drugi wykres funkcji sinus do zadania 3 i zmodyfikuj parametry funkcji,
+# tak aby osiągnąć efekt podobny do tego na poniższym zrzucie ekranu.
