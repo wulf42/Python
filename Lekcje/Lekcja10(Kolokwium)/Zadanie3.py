3a3b330c5c571c14cb3bd00cdda3af177cbfeeb6
Wulf <wojtekpako2@gmail.com>
2020-06-12 11:43:18 +0200

    .

--- Lekcje/Lekcja10(Kolokwium)/Zadanie3.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('cls')
-
-# 1 Pakoca Wojciech zestaw_20200513_ISI2_1
-
-lista = [2*x+3 for x in range(1, 11, 1)]
-print(lista)
--- Ćwiczenia/06 String Lists.py
+++
@@ -1,5 +0,0 @@
-a = input("Napisz coś: ")
-if a == a[::-1]:
-    print("palindrom")
-else:
-    print("niepalindrom ")
