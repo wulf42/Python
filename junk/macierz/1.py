e636f98f5a25911b8d5eab3dc5d0f114a9138d3e
Wulf <wojtekpako2@gmail.com>
2020-06-12 15:27:51 +0200

    commit 5

--- junk/macierz/1.py
+++ junk/macierz/1.py
@@ -5,9 +5,6 @@ os.system("cls")
 n = np.zeros((10, 10))
 for i in range(0, 10, 1):
     for j in range(0, 10, 1):
-        if (i == j):
+        if i == j:
             n[i][j] = 10 - i
 print(n)
-
-
-print("zmiana bardzo ważna")
