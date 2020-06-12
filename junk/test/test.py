3f28dc2eda416505d0e02c26e99c5b0b6132afda
Wulf <wojtekpako2@gmail.com>
2020-06-12 12:37:30 +0200

    .test

--- junk/test/test.py
+++ junk/test/test.py
@@ -1,6 +1,9 @@
-print("Test")
-print("Siemka")
-print("Siemka")
-print("Siemka")
-print("Siemka")
-print("Siemka")
+import matplotlib.pyplot as plt
+import numpy as np
+
+dziedzina = np.arange(0, 12, 0.1)
+s = np.sin(dziedzina)
+plt.plot(dziedzina, s, label='f(x)= sin(x)')
+plt.plot(dziedzina, -s, '--', label='f(x)= -sin(x)')
+plt.legend(loc=4)  # loklaizacja legendy
+plt.show()
