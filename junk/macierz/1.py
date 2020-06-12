5d876760e7f4613fac8cf23e7c3fe6d27f71e579
Wulf <wojtekpako2@gmail.com>
2020-06-12 14:37:11 +0200

    commit 4

--- junk/macierz/1.py
+++ junk/macierz/1.py
@@ -1,8 +1,13 @@
 import numpy as np
+import os
 
-n = np.zeros((10,10))
-for i in range(0, 10,1):
-    for j in range(0, 10,1):
-        if(i==j):
-            n[i][j] = 10-i
+os.system("cls")
+n = np.zeros((10, 10))
+for i in range(0, 10, 1):
+    for j in range(0, 10, 1):
+        if (i == j):
+            n[i][j] = 10 - i
 print(n)
+
+
+print("zmiana bardzo ważna")
